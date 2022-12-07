a764fdd049e7826c595818956f6cfb65bdfa3eb8
Jamie Vaughn <wxv6@case.edu>
2022-12-07 00:05:47 -0500

    more notes

--- edx.hs
+++ edx.hs
@@ -1,11 +1,14 @@
 -- quick sort
-qs :: (Ord a) => [a] -> [a]  
+qs :: (Ord a) => [a] -> [a]  -- types begin with upper case (Ord)
 qs [] = []
-qs (x:xs) = qs ys ++ [x] ++ f zs
-           where
+qs (x:xs) = qs ys ++ [x] ++ f zs 
+           where -- layout rule: whitespace is significant
               ys = [a | a <- xs, a <= x]
               zs = [b | b <- xs, b > x]
-              
+
+xs -- by convention list args have an "s" suffix
+nss -- list of lists
+
 head [1,2,4,5,6,]  -- 1
 tail [1,2,3,4,5] -- 5
 [1,2,3,4,5] !! 2 -- 3
@@ -17,6 +20,8 @@ product [1,2,3]
 [1] ++ [2] -- [1, 2]
 reverse [0,1] -- [1,0]
 
+-- function & arg identifiers must start with lowercase
+f' a = a * a
 -- function application denoted with spaces
 f a b + c * d -- f(a,b) + c * d 
 
@@ -28,4 +33,25 @@ f x * g y -- f(x) * g(y)
 
 -- infix function
 add x y = x + y
-x `add` y -- bckticks make add infix
+x `add` y -- backticks make add infix
+
+-- Booleans aka Bool
+not True || False -- capital letter identifiers are special data constructors
+
+-- Strings aka [Char]
+"Hello" ++ " World"
+length "Hello" -- can also use head, tail, last, init, reverse, null builtin functions on Strings
+'H' -- single Char uses single quotes
+"Hello" -- list of Chars uses double quotes (aka String)
+
+-- Errors
+-- static errors happen at compile time 
+-- dynamic errors happen at runtime -- head ""
+-- lexical error is a static error regarding syntax -- "Hello
+-- type error indicate a semantic fault like providing the wrong type to a function -- not "Hello"
+
+-- Types
+:t True -- finds the type True :: Bool, "Hi" :: [Char] <- list of Chars
+:t not -- not :: Bool -> Bool
+:t length -- [a] -> Int -- a is a generic type
+[True, "hello"] -- type error, lists must be of the same type
